3feca56620f93ed95cfc979884b478ae0ba14850
Abramov276 <vabramov276@gmail.com>
2024-10-17 18:03:32 +0300

    Add files via upload

---
+++ dz_cats.py
@@ -0,0 +1,23 @@
+def get_cats_info(path):
+    cats_info = []
+
+    with open('cats.txt', 'r') as cat:
+
+        for line in cat:
+            line = line.strip()
+            cats = line.split(',')
+
+            if len(cats) > 1:
+                cats_dict = {
+                    'id': cats[0],
+                    'name': cats[1],
+                    'age': cats[2]
+                }
+                cats_info.append(cats_dict)
+
+            else:
+                print(f'Помилка у рядку {line}')
+    return cats_info
+
+
+print(get_cats_info('cats.txt'))
---
+++ dz_salary.py
@@ -0,0 +1,26 @@
+def total_salary(path):
+    with open('salary.txt', 'r', encoding='utf-8') as sal:
+        salaries = []
+        for line in sal:
+            path = line.split(',')
+
+            if len(path) > 1:
+                name, salary = path
+
+                try:
+                    salaries.append(int(salary.strip()))
+
+                except ValueError:
+                    print(f"Invalid format for {name.strip()}")
+
+    if salaries:
+        total_salary = sum(salaries)
+        average_salary = total_salary / len(salaries)
+
+        return f"The total amount of salary: {
+            total_salary}, Average salary: {average_salary}"
+    else:
+        return 'No file'
+
+
+print(total_salary('salary.txt'))
